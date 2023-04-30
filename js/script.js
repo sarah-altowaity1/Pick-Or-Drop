2b71cbcae21dd219f84e8148b20fff01293d5935
sarah-altowaity1 <sa5793@nyu.edu>
2023-05-01 00:27:26 +0400

    added nav icon as buttons

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var score =  document.getElementById('score');
 var signal =  document.getElementById('signal');
 var s = 0
 var num_clicked = 0 
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < 13; i++) {
   // Create a new image element
 
   var img = document.createElement('img');
@@ -21,12 +21,12 @@ for (var i = 0; i < 10; i++) {
   // var maxHeight = rect.height + img.height;
   // var randomTop = rect.top + Math.floor(Math.random() *(rect.bottom - rect.top + 1));
   // img.style.top = randomTop + 'px';
-  img.style.left = Math.floor(Math.random() *(30)) + '%';
+  img.style.left = Math.floor(Math.random() *(10)) + '%';
   // // Calculate a random left position within the bounds of the display window
   // var maxWidth = rect.width - img.width;
   // var randomLeft = rect.left + Math.floor(Math.random() * (rect.right - rect.left + 1));
   // img.style.left = randomLeft + 'px';
-  img.style.top = Math.floor(Math.random() *(50)) + '%';
+  img.style.top = 10 + Math.floor(Math.random() *(50)) + '%';
   // Add a click event listener to the image
   img.addEventListener('click', function() {
     this.style.visibility = 'hidden';
@@ -41,11 +41,12 @@ for (var i = 0; i < 10; i++) {
     else{
       s = s + 1;
     }
-    score.textContent = "$$$: " + s; 
+    // score.textContent = "$$$: " + s; 
     num_clicked += 1; 
 
-    if (num_clicked == 10){
+    if (num_clicked == 13){
         signal.textContent = "You stumbled on $"+ s + " dollars. Refresh the page to see what luck gets you next!"
+        signal.style.display = 'block';
     }
   });
 
